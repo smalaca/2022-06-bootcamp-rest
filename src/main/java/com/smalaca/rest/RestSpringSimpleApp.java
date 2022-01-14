22fefe9c2d7bf8a60f71be21a22787527880409d
smalaca <ab13.krakow@gmail.com>
2022-01-14 10:19:38 +0100

    Initial commit

---
+++ src/main/java/com/smalaca/rest/RestSpringSimpleApp.java
@@ -0,0 +1,11 @@
+package com.smalaca.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringSimpleApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RestSpringSimpleApp.class, args);
+    }
+}
