ccc1d51d14de83d5eafc0433b388c0ce636998e6
smalaca <ab13.krakow@gmail.com>
2022-06-04 10:04:20 +0200

    use optional read parameter

--- src/main/java/com/smalaca/restapp/api/rest/HelloRestController.java
+++ src/main/java/com/smalaca/restapp/api/rest/HelloRestController.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
 
-    @GetMapping("/{name}")
+    @GetMapping(value = {"/", "/{name}"})
 //    @RequestMapping(method = {RequestMethod.GET})
-    public String hello(@PathVariable String name) {
-        return "Hello " + name + "!";
+    public String hello(@PathVariable Optional<String> name) {
+        if (name.isEmpty()) {
+            return "Hello world!";
+        } else {
+            return "Hello " + name.get() + "!";
+        }
     }
 }
