a2058b43072f171fd1eded6fa4d4108e6a58d837
smalaca <ab13.krakow@gmail.com>
2022-06-04 09:52:51 +0200

    read login and luckyNumber variables from url

--- src/main/java/com/smalaca/restapp/api/rest/FunRestController.java
+++ src/main/java/com/smalaca/restapp/api/rest/FunRestController.java
@@ -1,6 +1,7 @@
-package com.smalaca.rest.api.rest;
+package com.smalaca.restapp.api.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class FunRestController {
     @GetMapping
     public String isFun() {
         return "REST is fun!";
+    }
+
+    @GetMapping("/{login}/{luckyNumber}")
+    public String luckyNumber(@PathVariable String login, @PathVariable int luckyNumber) {
+        return "Cześć, jestem " + login + ". Moja szczęśliwa liczba to: " + luckyNumber;
     }
 }
